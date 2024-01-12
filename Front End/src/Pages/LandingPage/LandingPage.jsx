74339b1237006b0410079a6c4770aa75746aa6ae
sagini18 <saginisaju@gmail.com>
2024-01-12 14:36:54 +0530

    image update

--- Front End/src/Pages/LandingPage/LandingPage.jsx
+++ Front End/src/Pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuthContext } from '@asgardeo/auth-react';
-import './LandingPage.css'; // Import the CSS file
+import './LandingPage.css'; 
 
 const LandingPage = () => {
   const { signIn} = useAuthContext();
@@ -15,7 +15,7 @@ const LandingPage = () => {
               {/* <img src="https://firebasestorage.googleapis.com/v0/b/web-login-7e719.appspot.com/o/logo%20full.png?alt=media&token=6ac63cc0-86bb-4333-85b9-bd63cb3b6df6"
                alt="Welcome." className="welcome-image" /> */}
                <img src='https://firebasestorage.googleapis.com/v0/b/web-login-7e719.appspot.com/o/Certification.gif?alt=media&token=2e27c604-5df7-469e-aa3d-efe035d5a944'
-               className='welcome-image'/>
+              alt='landing-page-welcome-image' className='welcome-image'/>
               <div className="welcome-text">
                 <p className="welcome-header">
                 Welcome to Grama Check! 
