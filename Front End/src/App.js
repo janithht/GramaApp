0ba75d9ef7a56e83cf4390e7d922adcba54b4a4f
thiunuwan <ravinduthiunuwan@gmail.com>
2024-01-18 13:04:35 +0530

    Update routes

--- Front End/src/App.js
+++ Front End/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import Landing from "./Pages/Landing/landing.jsx";
 import Dashboard from "./Pages/Dashboard/dashboard.jsx";
 import Application from "./Pages/Application/application.jsx";
-import GramaDashboard from "./Pages/GramaDashboard/gramaDashboard.jsx";
 import SlackSupport from "./Pages/SlackSupport/slackSupport.jsx";
+import AdminDashboard from "./Pages/AdminDashboard/adminDashboard.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/application" element={<Application />} />
-          <Route path="/grama-dashboard" element={<GramaDashboard />} />
+          <Route path="/grama-dashboard" element={<AdminDashboard />} />
           <Route path="/slack-support" element={<SlackSupport/>}></Route>
         </Routes>
       </Router>
