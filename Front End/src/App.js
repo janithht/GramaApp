51e9982643523acb137e56cc4ec6c77aced48239
Nethmi Kavindya <kavindya.kdn@gmail.com>
2024-01-18 10:19:58 +0530

    application form
    application form finalized

--- Front End/src/App.js
+++ Front End/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Landing from "./Pages/Landing/landing.jsx";
 import Dashboard from "./Pages/Dashboard/dashboard.jsx";
 import Application from "./Pages/Application/application.jsx";
-import GramaDashboard from "./Pages/Grama-Dashboard/gramaDashboard.jsx";
+// import GramaDashboard from "./Pages/Grama-Dashboard/gramaDashboard.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/application" element={<Application />} />
-          <Route path="/grama-dashboard" element={<GramaDashboard />} />
+          {/* <Route path="/grama-dashboard" element={<GramaDashboard />} /> */}
           <Route path="/" element={<Landing />} />
         </Routes>
       </Router>
--- Front End/src/Components/MenuBar/menubar.jsx
+++ Front End/src/Components/MenuBar/menubar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './menubar.css'; 
 import Button from "../Button/button"
 import { useAuthContext } from '@asgardeo/auth-react';
-
+import { useNavigate } from 'react-router-dom';
 
 const MenuBar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -13,11 +13,14 @@ const MenuBar = () => {
 
   const { signOut} = useAuthContext();
 
+  const navigate = useNavigate();
+
+
   return (
     <div className="menu-bar">
       <p className='grama-name'>Grama Check</p>
       <div className="nav-menu">
-        <Button className='menu-btn'>Main Menu</Button>
+        <Button className='menu-btn' onClick={()=> navigate("/dashboard")}>Main Menu</Button>
         <button className='menu-btn' onClick={() => signOut()}>Logout</button>
       </div>
       <div className="menu-icon" onClick={toggleMobileMenu}>
@@ -27,7 +30,7 @@ const MenuBar = () => {
       </div>
       {isMobileMenuOpen && (
         <div className="mobile-menu">
-          <button className="menu-item">Main Menu</button>
+          <button className="menu-item" onClick={()=> navigate("/dashboard")}>Main Menu</button>
           <button className="menu-item" onClick={()=>signOut()}>Logout</button>
         </div>
       )}
