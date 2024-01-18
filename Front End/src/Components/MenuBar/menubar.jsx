d8f84062892893ff3edcf553da57bf8dc0f563ac
Nethmi Kavindya <kavindya.kdn@gmail.com>
2024-01-18 10:55:49 +0530

    menu bar update

--- Front End/src/Components/MenuBar/menubar.jsx
+++ Front End/src/Components/MenuBar/menubar.jsx
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import './menubar.css'; 
-import Button from "../Button/button"
-import { useAuthContext } from '@asgardeo/auth-react';
+import React, { useEffect, useState } from "react";
+import "./menubar.css";
+import Button from "../Button/button";
+import { useAuthContext } from "@asgardeo/auth-react";
 import { useNavigate } from 'react-router-dom';
 
 const MenuBar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { getBasicUserInfo } = useAuthContext();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    getBasicUserInfo()?.then((user) => {
+      setUser(user);
+    });
+  }, [getBasicUserInfo]);
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  const { signOut} = useAuthContext();
+  const { signOut } = useAuthContext();
 
   const navigate = useNavigate();
 
 
   return (
     <div className="menu-bar">
-      <p className='grama-name'>Grama Check</p>
+      <p className="grama-name">Grama Check</p>
       <div className="nav-menu">
         <Button className='menu-btn' onClick={()=> navigate("/dashboard")}>Main Menu</Button>
-        <button className='menu-btn' onClick={() => signOut()}>Logout</button>
+        <Button className='menu-btn' onClick={() => signOut()}>Logout</Button>
       </div>
       <div className="menu-icon" onClick={toggleMobileMenu}>
         <span className="menu-dot"></span>
@@ -30,8 +38,12 @@ const MenuBar = () => {
       </div>
       {isMobileMenuOpen && (
         <div className="mobile-menu">
-          <button className="menu-item" onClick={()=> navigate("/dashboard")}>Main Menu</button>
-          <button className="menu-item" onClick={()=>signOut()}>Logout</button>
+          <button className="menu-item">Main Menu</button>
+          {/* <MdContactSupport className="support-icon" size={35} />  */}
+          <button className="menu-item">Support</button>
+          <button className="menu-item" onClick={() => signOut()}>
+            Logout
+          </button>
         </div>
       )}
     </div>
