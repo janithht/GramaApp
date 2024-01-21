725846fc792d71d4ab3096655651cc24d4552c0e
Nethmi Kavindya <kavindya.kdn@gmail.com>
2024-01-22 00:21:24 +0530

    application form changed

--- FrontEnd/src/Pages/Application/application.jsx
+++ FrontEnd/src/Pages/Application/application.jsx
@@ -1,32 +1,28 @@
-// Import necessary modules and components
 import React, { useState } from 'react';
 import MenuBar from '../../Components/MenuBar/menubar.jsx';
-import InputField from "../../Components/Input/input.jsx";
 import "./application.css";
 import applyCertificate from "../../Assets/Apply.png";
-import Button from "../../Components/Button/button.jsx";
-
-
+import CustomButton from "../../Components/CustomButton/custombutton.jsx";
+import validationSchema from "./validationSchema.js";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const Application = () => {
-  // State variables for form fields
-  const [name, setName] = useState('');
+  const [tele, setTele] = useState('');
   const [nic, setNIC] = useState('');
   const [addressNumber, setAddressNumber] = useState('');
   const [street1, setStreet1] = useState('');
   const [street2, setStreet2] = useState('');
   const [city, setCity] = useState('');
   const [gramaSevaDivision, setGramaSevaDivision] = useState('');
+  const [validationErrors, setValidationErrors] = useState({});
 
-  // Dropdown options for Grama Seva Division
   const gramaSevaDivisionOptions = [
     { value: 'division1', label: 'Division 1' },
     { value: 'division2', label: 'Division 2' },
-    // Add more options as needed
   ];
 
-  // Event handlers for form fields
-  const handleNameChange = (e) => setName(e.target.value);
+  const handleTeleChange = (e) => setTele(e.target.value);
   const handleNICChange = (e) => setNIC(e.target.value);
   const handleAddressNumberChange = (e) => setAddressNumber(e.target.value);
   const handleStreet1Change = (e) => setStreet1(e.target.value);
@@ -34,9 +30,31 @@ const Application = () => {
   const handleCityChange = (e) => setCity(e.target.value);
   const handleGramaSevaDivisionChange = (e) => setGramaSevaDivision(e.target.value);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    validationSchema.validate({
+      tele,
+      nic,
+      addressNumber,
+      street1,
+      street2,
+      city,
+      gramaSevaDivision,
+    }, { abortEarly: false })
+      .then(() => console.log('Form is valid. Submitting...'))
+      .catch((err) => {
+        const errors = {};
+        err.inner.forEach((error) => {
+          errors[error.path] = error.message;
+        });
+        setValidationErrors(errors);
+      });
+  };
+
   return (
     <div className='full-container'>
-        <MenuBar />
+      <MenuBar />
       <div className="main-form-container">
         <div className="img-container">
           <div className='heading-3'>
@@ -46,22 +64,126 @@ const Application = () => {
           <img className="side-image" src={applyCertificate} alt="Description" />
         </div>
         <div className="form-container">
-          <form className='application-form'>
-            <InputField type="text" placeholder="Name" value={name} onChange={handleNameChange} />
-            <InputField type="text" placeholder="NIC" value={nic} onChange={handleNICChange} />
-            <InputField type="text" placeholder="Address Number" value={addressNumber} onChange={handleAddressNumberChange} />
-            <InputField type="text" placeholder="Street 1" value={street1} onChange={handleStreet1Change} />
-            <InputField type="text" placeholder="Street 2" value={street2} onChange={handleStreet2Change} />
-            <InputField type="text" placeholder="City" value={city} onChange={handleCityChange} />
-            <InputField className='select-input' type="select" options={gramaSevaDivisionOptions} value={gramaSevaDivision} onChange={handleGramaSevaDivisionChange} />
-            <Button className="submit-button">Submit Application</Button>
+          <form className='application-form' onSubmit={handleSubmit}>
+            <div className="input-field-div" >
+              <InputGroup className={`mb-3 ${validationErrors.tele ? 'error' : ''}`}>
+                <InputGroup.Text id="name-label">Telephone Number</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  // placeholder="Telephone Number"
+                  value={tele}
+                  onChange={handleTeleChange}
+                  aria-label="Telephone Number"
+                  aria-describedby="name-label"
+                />
+              </InputGroup>
+              {validationErrors.tele && <div className="error-message">{validationErrors.tele}</div>}
+            </div>
+
+            <div className='input-field-div'>
+            <InputGroup className={`mb-3 ${validationErrors.nic ? 'error' : ''}`}>
+                <InputGroup.Text id="nic-label">NIC</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  // placeholder="NIC"
+                  value={nic}
+                  onChange={handleNICChange}
+                  aria-label="NIC"
+                  aria-describedby="nic-label"
+                />
+              </InputGroup>
+              {validationErrors.nic && <div className="error-message">{validationErrors.nic}</div>}
+            </div>
+
+            <div className='input-field-div'>
+            <InputGroup className={`mb-3 ${validationErrors.addressNumber ? 'error' : ''}`}>
+                <InputGroup.Text id="address-label">Address Number</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  // placeholder="Address Number"
+                  value={addressNumber}
+                  onChange={handleAddressNumberChange}
+                  aria-label="Address Number"
+                  aria-describedby="address-label"
+                />
+              </InputGroup>
+              {validationErrors.addressNumber && <div className="error-message">{validationErrors.addressNumber}</div>}
+            </div>
+
+            <div className='input-field-div'>
+            <InputGroup className={`mb-3 ${validationErrors.street1 ? 'error' : ''}`}>
+                <InputGroup.Text id="street1-label">Street 1</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  // placeholder="Street 1"
+                  value={street1}
+                  onChange={handleStreet1Change}
+                  aria-label="Street 1"
+                  aria-describedby="street1-label"
+                />
+              </InputGroup>
+              {validationErrors.street1 && <div className="error-message">{validationErrors.street1}</div>}
+            </div>
+
+            <div className='input-field-div'>
+            <InputGroup className={`mb-3 ${validationErrors.street2 ? 'error' : ''}`}>
+                <InputGroup.Text id="street2-label">Street 2</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  // placeholder="Street 2"
+                  value={street2}
+                  onChange={handleStreet2Change}
+                  aria-label="Street 2"
+                  aria-describedby="street2-label"
+                />
+              </InputGroup>
+              {validationErrors.street2 && <div className="error-message">{validationErrors.street2}</div>}
+            </div>
+
+            <div className='input-field-div'>
+            <InputGroup className={`mb-3 ${validationErrors.city ? 'error' : ''}`}>
+                <InputGroup.Text id="city-label">City</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  // placeholder="City"
+                  value={city}
+                  onChange={handleCityChange}
+                  aria-label="City"
+                  aria-describedby="city-label"
+                />
+              </InputGroup>
+              {validationErrors.city && <div className="error-message">{validationErrors.city}</div>}
+            </div>
+
+            <div className='input-field-div'>
+            <InputGroup className={`mb-3 ${validationErrors.gramaSevaDivision ? 'error' : ''}`}>
+                <InputGroup.Text id="division-label">Grama Seva Division</InputGroup.Text>
+                <Form.Control
+                  as="select"
+                  className="select-input"
+                  value={gramaSevaDivision}
+                  onChange={handleGramaSevaDivisionChange}
+                  aria-label="Grama Seva Division"
+                  aria-describedby="division-label"
+                >
+                  {gramaSevaDivisionOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </Form.Control>
+              </InputGroup>
+              {validationErrors.gramaSevaDivision && <div className="error-message">{validationErrors.gramaSevaDivision}</div>}
+            </div>
+
+            <CustomButton type="submit">Submit Application</CustomButton>
           </form>
         </div>
-        {/* Image on the left side */}
-        
       </div>
     </div>
   );
 };
 
 export default Application;
+
+
