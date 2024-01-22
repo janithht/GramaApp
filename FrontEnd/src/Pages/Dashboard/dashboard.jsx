f2746f3002707ceeba2f968e069330aadc3e9642
Nethmi Kavindya <kavindya.kdn@gmail.com>
2024-01-23 02:49:30 +0530

    Front end components created
    Chat modal and confirmation modal to separate files

--- FrontEnd/src/Pages/Dashboard/dashboard.jsx
+++ FrontEnd/src/Pages/Dashboard/dashboard.jsx
@@ -4,24 +4,13 @@ import "../../Components/MenuBar/menubar.css";
 import applyCertificate from "../../Assets/Apply.png";
 import statusCheck from "../../Assets/Status.png";
 import help from "../../Assets/Help.png";
-import Avatar from "../../Assets/Avatar.webp";
-import G_Logo from "../../Assets/G_Logo.png";
 import "./dashboard.css";
 import Tile from "../../Components/Tile/tile.jsx";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { MdContactSupport } from "react-icons/md";
 import moment from "moment";
-import {
-  CButton,
-  CModal,
-  CModalBody,
-  CModalFooter,
-  CModalHeader,
-  CModalTitle,
-  CSpinner,
-} from "@coreui/react";
 import { getToken } from "../../Utils/getToken.js";
+import ChatModal from "./chatmodal.jsx";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -142,87 +131,14 @@ const Dashboard = () => {
               <div className="tile-text">Ask for Help</div>
             </div>
           </div>
-          <CModal
-            scrollable
-            visible={visible}
-            onClose={() => setVisible(false)}
-            aria-labelledby="ScrollingLongContentExampleLabel2">
-            <CModalHeader>
-              <CModalTitle id="ScrollingLongContentExampleLabel2">
-                <MdContactSupport size={28} />
-                Grama Support
-              </CModalTitle>
-            </CModalHeader>
-            {loading?<CModalBody>
-              {chatHistory?.map((chat, index) => {
-                return (
-                  <span key={index}>
-                    {chatHistory[index - 1]?.date !==
-                      chatHistory[index]?.date && (
-                      <div className="date-divider">
-                        <span>{chat?.date}</span>
-                      </div>
-                    )}
-                    {chat?.user === "Grama Support" ? (
-                      <div className="ask-for-help-chat-right">
-                        <div className="chat-divider-right">
-                          <div>
-                            <span className="chat-username">
-                              {chat?.user} (citizen){" "}
-                            </span>
-                            <span className="chat-time">{chat?.time_only}</span>
-                          </div>
-                          <p className="chat-text chat-text-right">
-                            {chat?.message}
-                          </p>
-                        </div>
-                        <img
-                        src={G_Logo}
-                          alt="avatar"
-                          className="chat-avatar"
-                        />
-                      </div>
-                    ) : (
-                      <div className="ask-for-help-chat-left">
-                        <img
-                        src={Avatar}
-                          alt="avatar"
-                          className="chat-avatar"
-                        />
-                        <div className="chat-divider-left">
-                          <div>
-                            <span className="chat-username">
-                              {chat?.user} (Grama){" "}
-                            </span>
-                            <span className="chat-time">{chat?.time_only}</span>
-                          </div>
-                          <p className="chat-text chat-text-left">
-                            {chat?.message}
-                          </p>
-                        </div>
-                      </div>
-                    )}
-                  </span>
-                );
-              })}
-            </CModalBody>:
-            <CModalBody>
-              <div className="loading-spinner d-flex justify-content-center">
-                <CSpinner color="primary" />
-              </div>
-            </CModalBody>}
-            <CModalFooter className=" chat-message-send">
-              <input
-                type="text"
-                className="form-control"
-                placeholder={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-              <CButton color="primary" onClick={handleSendMessage}>
-                Send
-              </CButton>
-            </CModalFooter>
-          </CModal>
+          <ChatModal
+          visible={visible}
+          setVisible={setVisible}
+          loading={loading}
+          chatHistory={chatHistory}
+          message={message}
+          handleSendMessage={handleSendMessage}
+        />
         </div>
       </div>
     </div>
