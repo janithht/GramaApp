2a0347a94431d4ea62b230be7728a42119e79a66
sagini18 <saginisaju@gmail.com>
2024-01-23 10:51:12 +0530

    chat model get update

--- FrontEnd/src/Pages/Dashboard/chatmodel.jsx
+++ FrontEnd/src/Pages/Dashboard/chatmodel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import {
   CModal,
   CModalHeader,
@@ -13,15 +13,81 @@ import Avatar from "../../Assets/Avatar.webp";
 import G_Logo from "../../Assets/G_Logo.png";
 import axios from "axios";
 import { getToken } from "../../Utils/getToken.js";
+import moment from "moment";
 
-const ChatModal = ({ visible, setVisible, loading, chatHistory }) => {
-  const [message, setmessage] = useState("");
+
+const ChatModal = ({ visible, setVisible, }) => {
+  const [message, setMessage] = useState("");
   const [token, setToken] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleToken = async () => {
     const token = await getToken();
     setToken(token);
   };
+  
+  useEffect(() => {
+    setMessage("");
+    handleToken();
+    if (token !== "") {
+      axios
+        .get(
+          "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/bwsu/slackconnector-evm/slackservice-3b5/v1.0/getMessages",
+          {
+            headers: {
+              Accept: "application/scim+json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          setChatHistory([]);
+          res?.data?.map((chat) => {
+            const formatted = moment.unix(chat.timestamp).format("LLLL");
+            const current_date = moment().format("MMM Do YY");
+            const chat_date = moment(formatted).format("MMM Do YY");
+            var date_object = new Date(formatted);
+            var time_only = date_object.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            });
+            var date;
+            if (current_date !== chat_date) {
+              date = formatted.slice(0, -14);
+              if (date.endsWith(",")) {
+                date = formatted.slice(0, -15);
+              }
+              if (date.endsWith("1")) {
+                date += "st";
+              } else if (date.endsWith("2")) {
+                date += "nd";
+              } else if (date.endsWith("3")) {
+                date += "rd";
+              } else {
+                date += "th";
+              }
+            } else {
+              date = "Today";
+            }
+            setChatHistory((prev) => [
+              ...prev,
+              {
+                user: chat.user,
+                message: chat.message,
+                timestamp: formatted,
+                time_only,
+                date,
+              },
+            ]);
+          });
+          setLoading(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [visible]);
 
   const handleSendMessage = () => {
     handleToken();
@@ -39,7 +105,7 @@ const ChatModal = ({ visible, setVisible, loading, chatHistory }) => {
         )
         .then((res) => {
           setVisible(false);
-          setmessage("");
+          setMessage("");
         })
         .catch((err) => {
           console.log("error:", err);
@@ -116,7 +182,7 @@ const ChatModal = ({ visible, setVisible, loading, chatHistory }) => {
           type="text"
           className="form-control"
           placeholder={message}
-          onChange={(e) => setmessage(e.target.value)}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <CButton color="primary" onClick={handleSendMessage}>
           Send
--- FrontEnd/src/Pages/Dashboard/dashboard.jsx
+++ FrontEnd/src/Pages/Dashboard/dashboard.jsx
@@ -6,86 +6,12 @@ import statusCheck from "../../Assets/Status.png";
 import help from "../../Assets/Help.png";
 import "./dashboard.css";
 import Tile from "../../Components/Tile/tile.jsx";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import moment from "moment";
-import { getToken } from "../../Utils/getToken.js";
 import ChatModal from "./chatmodel.jsx";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const [message, setMessage] = useState("");
   const [visible, setVisible] = useState(false);
-  const [chatHistory, setChatHistory] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [token, setToken] = useState("");
-
-  const handleToken = async () => {
-    const token = await getToken();
-    setToken(token);
-  };
-
-  useEffect(() => {
-    setMessage("");
-    handleToken();
-    if (token !== "") {
-      axios
-        .get(
-          "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/bwsu/slackconnector-evm/slackservice-3b5/v1.0/getMessages",
-          {
-            headers: {
-              Accept: "application/scim+json",
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        )
-        .then((res) => {
-          setChatHistory([]);
-          res?.data?.map((chat) => {
-            const formatted = moment.unix(chat.timestamp).format("LLLL");
-            const current_date = moment().format("MMM Do YY");
-            const chat_date = moment(formatted).format("MMM Do YY");
-            var date_object = new Date(formatted);
-            var time_only = date_object.toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-            });
-            var date;
-            if (current_date !== chat_date) {
-              date = formatted.slice(0, -14);
-              if (date.endsWith(",")) {
-                date = formatted.slice(0, -15);
-              }
-              if (date.endsWith("1")) {
-                date += "st";
-              } else if (date.endsWith("2")) {
-                date += "nd";
-              } else if (date.endsWith("3")) {
-                date += "rd";
-              } else {
-                date += "th";
-              }
-            } else {
-              date = "Today";
-            }
-            setChatHistory((prev) => [
-              ...prev,
-              {
-                user: chat.user,
-                message: chat.message,
-                timestamp: formatted,
-                time_only,
-                date,
-              },
-            ]);
-          });
-          setLoading(true);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [visible]);
 
   return (
     <div className="full-container">
@@ -112,8 +38,6 @@ const Dashboard = () => {
           <ChatModal
           visible={visible}
           setVisible={setVisible}
-          loading={loading}
-          chatHistory={chatHistory}
         />
         </div>
       </div>
